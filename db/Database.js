74eb07e418ed4ad611bcddb822975052ec8bdddd
Pravin soni <soniipravin68@gmail.com>
2024-11-21 15:28:08 +0530

    added ssl

--- db/Database.js
+++ db/Database.js
@@ -5,7 +5,12 @@ const uri = process.env.MONGODB_URI;
 console.log("mongodb uri", uri);
 
 const connectDb = async () => {
-  await mongoose.connect(uri);
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+    sslValidate: false, // Add this if SSL certificate validation fails
+  });
   console.log("database connected");
 };
 
